ab748b41172c50d8766f47765c48095fe4ee85f2
Pikachú <engbethany4@gmail.com>
2024-05-27 13:32:35 +0000

    change to string

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from openai import OpenAI
 import random
 from dotenv import load_dotenv
-from flask_caching import Cache
+# from flask_caching import Cache
 import os
 
 # Load the environment variables from the .env file
@@ -26,7 +26,7 @@ class ImageGenerator(Resource):
     # @cache.memoize(timeout=20)
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('user_prompt', required=True, help='User prompt cannot be blank!')
+        parser.add_argument('user_prompt', required=True, type=str, help='User prompt cannot be blank!')
         args = parser.parse_args()
 
         descriptive_prompts_list = ["A surrealist oil painting of the Kenyan city {}, with cool tones (blues, greens) and set during a dramatic and high contrast thunderstorm.",
@@ -58,25 +58,25 @@ class ImageGenerator(Resource):
         # Combine user propmt and descriptive prompt
         def generate_complete_prompt(user_prompt):
             match user_prompt:
-                case 1:
+                case "1":
                     return baseline_prompt.format("Nairobi, in 2500") 
-                case 2:
+                case "2":
                     return baseline_prompt.format("Mombasa, in 2270")
-                case 3:
+                case "3":
                     return baseline_prompt.format("Kisumu, in 2095")
-                case 4:
+                case "4":
                     return baseline_prompt.format("Nakuru, in 2470")
-                case 5:
+                case "5":
                     return baseline_prompt.format("Eldoret, in 2150")
-                case 6:
+                case "6":
                     return baseline_prompt.format("Thika, in 3200")
-                case 7:
+                case "7":
                     return baseline_prompt.format("Machakos, in 2620")
-                case 8:
+                case "8":
                     return baseline_prompt.format("Kitale, in 3000")
-                case 9:
+                case "9":
                     return baseline_prompt.format("Malindi, in 2110")
-                case 10:
+                case "10":
                     return random.choice(feeling_lucky_prompts_list)
 
                 # User's open ended prompt
